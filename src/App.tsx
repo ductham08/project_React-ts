e7402e9dca773b0a1ef27adea92eff2df02cf1fa
Ductham08702 <102347246+Ductham08702@users.noreply.github.com>
2022-11-21 21:22:29 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <p>Home page</p>
+        </div>
+    );
+}
+
+export default App;
