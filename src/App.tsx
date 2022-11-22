7a2e87f1def05ab7f7afff9aba68faf643c13739
Ductham08702 <102347246+Ductham08702@users.noreply.github.com>
2022-11-22 23:42:23 +0700

    forgot password

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./components/admin/login";
+import Login from "./components/user/login";
+import Re_password from "./components/user/re_password";
 
 function App() {
     return (
         <div className="App">
             <Routes>
                 <Route path="login" element={<Login />}></Route>
+                <Route path="forgotpass" element={<Re_password />}></Route>
             </Routes>
         </div>
     );
---
+++ src/components/user/re_password.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import logo from "../../../public/image/logo.png"
+import bg_forgot from "../../../public/image/background_Repass.png"
+import "../../../public/css/re_password.css"
+
+type Props = {}
+
+const Re_password = (props: Props) => {
+    return (
+        <div className='login_page'>
+            <div className="left_content col-5">
+                <div className="logo_left col-3">
+                    <img src={logo} width="100%" alt="" />
+                </div>
+                <div className="content_left col-8">
+                    <form>
+                        <h5>Đặt lại mật khẩu</h5>
+                        <p>Vui lòng nhập email để đặt lại mật khẩu của bạn</p>
+                        <input type="text" />
+                        <div className="item_bottom">
+                            <button className='exit'>Hủy</button>
+                            <button className='next'>Tiếp tục</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className="right_content col-7">
+                <div className="bg_right col-9">
+                    <img src={bg_forgot} width="100%" alt="" />
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Re_password
