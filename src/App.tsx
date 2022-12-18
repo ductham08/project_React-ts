a6af029f58316a130065d80f047579e1d98d4f3f
Ductham08702 <102347246+Ductham08702@users.noreply.github.com>
2022-12-18 11:57:42 +0700

    add account

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ import Manage_acount from "./page/admin/setting/manage_account";
 import Manage_history from "./page/admin/setting/manage_history";
 import New_role_page from "./page/admin/setting/new_role";
 import Update_role from "./page/admin/setting/update_role";
+import Add_account from "./page/admin/setting/add_account";
 
 function App() {
     return (
@@ -57,6 +58,7 @@ function App() {
                         <Route path="role/new" element={<New_role_page />}></Route>
                         <Route path="role/:id" element={<Update_role />}></Route>
                         <Route path="account" element={<Manage_acount />}></Route>
+                        <Route path="account/new" element={<Add_account />}></Route>
                         <Route path="history" element={<Manage_history />}></Route>
                     </Route>
                 </Route>
